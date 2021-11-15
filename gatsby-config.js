1710ccb3f9e9e9d9414a12731a90a8d0766a6f02
Thomas Berhe Ghebreyohannes <thomasgyohannes@gmail.com>
2021-11-15 13:19:36 -0600

    plugin cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,8 +43,6 @@ module.exports = {
     siteUrl: config.siteUrl,
   },
   plugins: [
-    `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
-    `gatsby-plugin-styled-jsx-postcss`, // as above
     `gatsby-remark-images`,
     {
       resolve: `gatsby-plugin-layout`,
